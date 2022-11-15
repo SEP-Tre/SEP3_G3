dd64bfc7c28f0a29efda882b9c24c936c17fa376
Kamil <fischbach.kamil@gmail.com>
2022-11-15 10:57:06 +0100

    Refactoritng the repository.
    I have created new solution with two seprate solutions: Blazor and WebAPI. That way it is easier to navigate and explain. Now you can open the "rightoversBlazorNWEB" as a project in rider for it work correctly.

---
+++ [CODE]/rightoversBlazorNWEB/Domain/DTOs/FoodPostCreationDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.DTOs;
+
+public class FoodPostCreationDTO
+{
+    public FoodPostCreationDTO(string title, string category, string description, string pictureUrl,
+        int daysUntilExpired)
+    {
+        Title = title;
+        Category = category;
+        Description = description;
+        PictureUrl = pictureUrl;
+        DaysUntilExpired = daysUntilExpired;
+    }
+
+    public string Title { get; }
+    public string Category { get; }
+    public string Description { get; }
+    public string PictureUrl { get; }
+    public int DaysUntilExpired { get; }
+}
