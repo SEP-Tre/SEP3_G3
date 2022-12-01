b6288b3290b5ebc0341c0e739afb063784a3b1ea
Kamil <fischbach.kamil@gmail.com>
2022-12-01 11:10:20 +0100

    somethingg

--- [CODE]/rightoversBlazorNWEB/Domain/Classes/FoodPost.cs
+++ [CODE]/rightoversBlazorNWEB/Domain/Classes/FoodPost.cs
@@ -10,18 +10,18 @@ public class FoodPost
         MyTime startTime, MyTime endTime, User user)
     {
         PostId = postId;
-        Title = title;
-        Category = category;
+        Title = title;//1
+        Category = category;//2
         Description = description;
-        PictureUrl = pictureUrl;
+        PictureUrl = pictureUrl;//3
         DaysUntilExpired = daysUntilExpired;
         PostState = postState;
         TimestampPosted = timestampPosted;
         StartDate = startDate;
         EndDate = endDate;
-        StartTime = startTime;
-        EndTime = endTime;
-        User = user;
+        StartTime = startTime;//4 togela
+        EndTime = endTime;//4 togela
+        User = user;//5 username
     }
 
     public FoodPost()
